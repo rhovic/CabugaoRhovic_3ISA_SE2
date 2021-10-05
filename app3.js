ef387af1739849af23fe3636cb67cc4f39445999
rhovic <60631854+rhovic@users.noreply.github.com>
2021-10-05 09:52:05 +0800

    added app3.js

---
+++ app3.js
@@ -0,0 +1,4 @@
+const search1 = require('./js/script3');
+
+let a = new Object("item1", "item2", "item3");
+console.log(search1("item4", a));
---
+++ js/script3.js
@@ -0,0 +1,13 @@
+let item = 0;
+let arr = 0;
+
+function search(item, arr){
+    this.item = item;
+    this.arr = arr;
+    return indexOf(item, 0);
+}
+
+module.exports = {
+    item:item,
+    arr:arr
+}
